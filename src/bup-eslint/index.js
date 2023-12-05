635c7099acc6830b9d473d54e3e66c136f51c03d
newObjectccc <897333949@qq.com>
2023-12-05 11:21:19 +0800

    fix(bup-eslint): fix eslint code

--- src/bup-eslint/index.js
+++ src/bup-eslint/index.js
@@ -3,16 +3,20 @@ import { Command } from 'commander';
 import ora from 'ora';
 import chooseFramework from '../common/choose-framework.js';
 import choosePkgMgr from '../common/choose-pkg-manager.js';
-import { ESLINT_FORMAT_TYPE } from '../helper/constant.js';
+import installPlugin from '../common/install-plugin.js';
+import { DEPS_NEED_TO_INSTALL, ESLINT_FORMAT_TYPE } from '../helper/constant.js';
 import { stderrHdr, stdoutHdr } from '../helper/output.js';
 import { installEslint, settingEslintrc } from './eslint-common.js';
 const program = new Command();
 
+const loadingEslintOra = ora({
+  text: 'Download eslint',
+});
 const settingEslintOra = ora({
   text: `Setting eslint...`,
 });
-const loadingEslintOra = ora({
-  text: 'Download ESLint',
+const downloadPluginOra = ora({
+  text: 'Download plugin',
 });
 
 program
@@ -32,15 +36,25 @@ program
       await installEslint({ pkgManager, stdoutHdr: (data) => stdoutHdr(loadingEslintOra, data) })
       loadingEslintOra.succeed('ESLint download succeed');
 
-      const fwk = chooseFramework()
+      // choose framework
+      const fwk = await chooseFramework()
+
       // setting eslintrc
       settingEslintOra.prefixText = chalk.dim('[info]');
       settingEslintOra.start()
       settingEslintOra.spinner = 'moon'
       settingEslintOra.text = chalk.green('Setting ESLint...')
-      const writeRes = await settingEslintrc({ fmt: fmt.format, fwk })
+      const writeRes = await settingEslintrc({ fmt: fmt.format, fwk: fwk.framework })
       settingEslintOra.succeed(`${writeRes}, completed!`)
+
+      // install plugin
+      downloadPluginOra.prefixText = chalk.dim('[download]');
+      downloadPluginOra.start()
+      downloadPluginOra.spinner = 'moon'
+      const installPlugRes = await installPlugin({ pkgManager, stdoutHdr: (data) => stdoutHdr(downloadPluginOra, data), plugin: DEPS_NEED_TO_INSTALL[fwk.framework] })
+      downloadPluginOra.succeed(`${installPlugRes}, all completed!`)
     } catch (error) {
+      console.log(error, '====>');
       stderrHdr(loadingEslintOra)
       stderrHdr(settingEslintOra)
     }
