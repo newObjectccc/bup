f27506a65ed619d9ece8596ddd6d7e6192561c14
newObjectccc <897333949@qq.com>
2023-12-05 11:22:41 +0800

    feat(helper): add contant type

--- src/helper/constant.js
+++ src/helper/constant.js
@@ -1,8 +1,13 @@
 
-export const ESLINT_FORMAT_TYPE = ['js', 'cjs', 'json', 'yaml', 'yml', 'pkg']
+export const ESLINT_FORMAT_TYPE = ['js', 'mjs', 'json', 'yml']
 
 export const CMD_ON_PKG_MANAGER = {
-  yarn: 'yarn add eslint -D',
-  npm: 'npm install eslint -D',
-  pnpm: 'pnpm add eslint -D',
+  pnpm: (plg) => `pnpm add ${plg} -D`,
+  yarn: (plg) => `yarn add ${plg} -D`,
+  npm: (plg) => `npm install ${plg} -D`,
+}
+
+export const DEPS_NEED_TO_INSTALL = {
+  vue: ['babel-eslint', 'eslint-plugin-vue'],
+  react: ['eslint-plugin-react']
 }
