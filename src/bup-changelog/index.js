7b580f984b39615b65e274cac65e4e6de60e572b
newObjectccc <897333949@qq.com>
2023-12-07 12:02:58 +0800

    fix(bup-changelog): bug fix

--- src/bup-changelog/index.js
+++ src/bup-changelog/index.js
@@ -1,21 +1,22 @@
-import { Command } from 'commander';
-import choosePkgMgr from '../common/choose-pkg-manager.js';
-import installPlugin from '../common/install-plugin.js';
-import isFileExistInRoot from '../common/is-file-exist.js';
-import { startOraWithTemp, stderrHdr, stdoutHdr } from '../helper/output.js';
-import { settingChangelogOptions } from './changelog-common.js';
+const Command = require('commander');
+import choosePkgMgr from '../common/choose-pkg-manager';
+import installPlugin from '../common/install-plugin';
+import isFileExistInRoot from '../common/is-file-exist';
+import { startOraWithTemp, stderrHdr, stdoutHdr } from '../helper/output';
+import { settingChangelogOptions } from './changelog-common';
 const program = new Command();
 
 program
   .option('-c, --custom', 'if specify, will create standard-version-updater.js to customize')
   .action(async ({ custom }) => {
+    let downloadPluginOra
     try {
       if (!isFileExistInRoot('package.json')) throw new Error('There is no package.json in the current folder!');
       // chiose pakcage manager
       const { pkgManager } = await choosePkgMgr()
 
       // install standard-version
-      const downloadPluginOra = startOraWithTemp('Download plugin...')
+      downloadPluginOra = startOraWithTemp('Download plugin...')
       const installPlugRes = await installPlugin({
         pkgManager,
         stdoutHdr: (data) => stdoutHdr(data, downloadPluginOra),
@@ -26,7 +27,8 @@ program
       // setting husky
       await settingChangelogOptions(pkgManager, custom)
     } catch (error) {
-      stderrHdr(error, downloadPluginOra)
+downloadPluginOra?.fail()
+      stderrHdr(error)
     }
   });
 
