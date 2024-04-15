2209deb92799f381e4b5e798ce8dc506841be07b
newObjectccc <897333949@qq.com>
2024-04-15 13:06:54 +0800

    fix(boilerplate): update git initial

--- src/boilerplate/index.js
+++ src/boilerplate/index.js
@@ -1,18 +1,15 @@
 const fs = require('fs');
-const isFileExistInRoot = require('../common/is-file-exist.js');
 const asyncOutput = require('../helper/output.js');
 const choicesPrompt = require('../common/choices-prompt.js');
 const execCmd = require('../common/exec-cmd.js');
 
-const BASE_CLONE_URL = 'https://github.com/newObjectccc/';
+const BASE_CLONE_URL = 'https://github.com/newObjectccc';
 
 const boilerplate = {
   action: async () => {
     let cloneBoilerplateOra, downloadPluginOra;
     const { startOraWithTemp, stderrHdr, stdoutHdr } = await asyncOutput();
     try {
-      if (!isFileExistInRoot('package.json'))
-        throw new Error('There is no package.json in the current folder!');
       const { framework } = await choicesPrompt('framework', [
         { title: 'vscode-boilerplate', value: 'vscode-extension-boilerplate' },
         { title: 'chrome-boilerplate', value: 'chrome-extension-boilerplate' },
@@ -20,17 +17,22 @@ const boilerplate = {
         { title: 'next-gen-web-boilerplate', value: 'next-gen-web-project' }
       ]);
       await execCmd({
-        cmdStr: `git clone ${BASE_CLONE_URL}${framework} .`,
+        cmdStr: `git clone ${BASE_CLONE_URL}/${framework}.git .`,
         stdoutHdr: (data) => stdoutHdr(data, downloadPluginOra),
         errMsg: `Failed to clone ${framework}!`
       });
       cloneBoilerplateOra = startOraWithTemp('boilerplate has installed!');
-      fs.rmdirSync('.git', { recursive: true });
+      fs.rmSync('.git', { recursive: true });
+      await execCmd({
+        cmdStr: 'git init && git add .',
+        stdoutHdr: (data) => stdoutHdr(data, cloneBoilerplateOra),
+        errMsg: 'Failed to init git!'
+      });
       cloneBoilerplateOra.succeed('boilerplate initail successed!');
     } catch (error) {
+      stderrHdr(error);
       downloadPluginOra?.fail();
       cloneBoilerplateOra?.fail();
-      stderrHdr(error);
     }
   }
 };
